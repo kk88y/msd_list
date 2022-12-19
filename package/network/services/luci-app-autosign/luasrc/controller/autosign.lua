629e1098e1990f051a9a09651754612cec853911
kk88y <61588462+kk88y@users.noreply.github.com>
2022-12-19 22:40:15 +0800

    Delete package/network/services/luci-app-autosign directory

--- package/network/services/luci-app-autosign/luasrc/controller/autosign.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.autosign",package.seeall)
-
-function index()
-	local page = entry({"admin", "services", "autosign"}, cbi("autosign"), _("AutoSign"))
-	page.dependent = true
-end
